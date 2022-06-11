795521276d496b45f9d2c8749f62f24979f311ee
Hakan ÖZER <hakanozer02@gmail.com>
2022-06-11 14:16:16 +0300

    Database Class and Security

--- Java/9-Days/src/main/java/org/example/App.java
+++ Java/9-Days/src/main/java/org/example/App.java
@@ -1,13 +1,18 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.List;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        UserService service = new UserService();
+        boolean status = service.userLogin("erkan@mail.com", "54321");
+        System.out.println("Login : " + status);
+
+        List<User> ls = service.allUser();
+        for ( User item : ls ) {
+            System.out.println( item );
+        }
     }
 }
