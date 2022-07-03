b0cd7003ca4b7f80c09ba60d399186f121073b14
Hakan ÖZER <hakanozer02@gmail.com>
2022-07-03 10:01:53 +0300

    Tekrar

--- Spring/Tekrar/src/main/java/com/works/entities/Note.java
+++ Spring/Tekrar/src/main/java/com/works/entities/Note.java
@@ -1,6 +1,8 @@
 package com.works.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
