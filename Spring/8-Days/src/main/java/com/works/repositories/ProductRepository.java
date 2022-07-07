4dfc9e0855745c3fc1fcbbe305065781aef57f51
Hakan ÖZER <hakanozer02@gmail.com>
2022-07-07 22:15:34 +0300

    Update ProductRepository.java

--- Spring/8-Days/src/main/java/com/works/repositories/ProductRepository.java
+++ Spring/8-Days/src/main/java/com/works/repositories/ProductRepository.java
@@ -4,4 +4,7 @@ import com.works.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    
+
 }
