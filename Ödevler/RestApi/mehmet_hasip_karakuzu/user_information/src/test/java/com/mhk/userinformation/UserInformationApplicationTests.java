311a11f8f1305d3dce19437d3505c8b068bcf9d4
Mehmet Hasip Karakzu <mhkrkzu@gmail.com>
2022-07-20 21:49:00 +0300

    UserInformationApp

---
+++ Ödevler/RestApi/mehmet_hasip_karakuzu/user_information/src/test/java/com/mhk/userinformation/UserInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhk.userinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInformationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
