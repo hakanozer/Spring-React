462d5eb3b66246a87bc92386e6f24ed0ccca11a2
Mehmet Hasip Karakzu <mhkrkzu@gmail.com>
2022-07-20 22:13:17 +0300

    Revert "Merge branch 'master' of https://github.com/hakanozer/Spring-React"
    This reverts commit b80a14101934d602d99808ebe99f56f16ec99256, reversing
    changes made to 311a11f8f1305d3dce19437d3505c8b068bcf9d4.

--- Spring/8-Days/src/main/java/com/works/services/ProductService.java
+++ Spring/8-Days/src/main/java/com/works/services/ProductService.java
@@ -3,9 +3,6 @@ package com.works.services;
 import com.works.entities.Product;
 import com.works.repositories.JoinProCatRepository;
 import com.works.repositories.ProductRepository;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -46,36 +43,7 @@ public class ProductService {
         return new ResponseEntity(hm, HttpStatus.OK);
     }
 
-    public ResponseEntity pageProduct( int start, int count ) {
-        Map<String, Object> hm = new LinkedHashMap<>();
-        if ( count > 5 ) {
-            count = 5;
-        }
-        hm.put("status", true);
-        hm.put("total", jRepo.totalSize());
-        hm.put("result", jRepo.pageProduct(start * count, count));
-        return new ResponseEntity(hm, HttpStatus.OK);
-    }
 
 
-
-    public ResponseEntity searchPage( String q, int page, int size ) {
-        Map<String, Object> hm = new LinkedHashMap<>();
-        hm.put("status", true);
-        if ( size > 5 ) {
-            size = 5;
-        }
-        Pageable pageable = PageRequest.of(page, size, Sort.by("price").ascending() );
-        hm.put("total", jRepo.countTotal("%" + q + "%"));
-        hm.put("result", jRepo.findAllPage("%" + q + "%" ,pageable) );
-        return new ResponseEntity(hm, HttpStatus.OK);
-    }
-
-    public ResponseEntity allProCatID( int cid ) {
-        Map<String, Object> hm = new LinkedHashMap<>();
-        hm.put("status", true);
-        hm.put("result", pRepo.allProCatID( cid ) );
-        return new ResponseEntity(hm, HttpStatus.OK);
-    }
 
 }
--- Ödevler/RestApi/Abdullah_Senturk/Spring-UserRestApi/src/main/java/com/works/tekrarproject/utils/ERest.java
+++
@@ -1,8 +0,0 @@
-package com.works.tekrarproject.utils;
-
-public enum ERest {
-
-    // Enum'lar rest için yazıldığında büyük harfle yazılmamalıdır!
-    result, status, message, errors;
-
-}
