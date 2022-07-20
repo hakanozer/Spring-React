10f26a60bb92d90ea62b5a5b9fa7675a1871961f
Fazli Gungor <fzlgungor@gmail.com>
2022-07-20 20:29:46 +0300

    Gun 9 Odev

---
+++ Ödevler/RestApi/Fazli_Gungor/OdevGun9/src/test/java/com/example/odevgun9/OdevGun9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.odevgun9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdevGun9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
