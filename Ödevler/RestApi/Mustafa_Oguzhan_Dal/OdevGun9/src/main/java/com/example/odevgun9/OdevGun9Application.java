6e4cc22c135339ae3320897e9d4176a92c4ba92a
Oguzhan DAL <daloguz11@hotmail.com>
2022-07-20 17:57:39 +0300

    RestTemplate ile başka apiyi tüketme

---
+++ Ödevler/RestApi/Mustafa_Oguzhan_Dal/OdevGun9/src/main/java/com/example/odevgun9/OdevGun9Application.java
@@ -0,0 +1,13 @@
+package com.example.odevgun9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdevGun9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdevGun9Application.class, args);
+    }
+
+}
