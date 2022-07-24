ec7e3c957de2ab56df658fdc93220d03f0ddf97b
Hakan ÖZER <hakanozer02@gmail.com>
2022-07-24 14:13:08 +0300

    @RestControllerAdvice

--- Sınavlar/Vize-1/Uygulama/src/main/java/com/works/restcontroller/TeamsCreateRestController.java
+++ Sınavlar/Vize-1/Uygulama/src/main/java/com/works/restcontroller/TeamsCreateRestController.java
@@ -4,10 +4,8 @@ import com.works.repositories.FootTeamRepository;
 import com.works.utils.TEnum;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/teams")
@@ -21,6 +19,12 @@ public class TeamsCreateRestController {
 
     @GetMapping("/teamCreate")
     public ResponseEntity teamCreate(@RequestParam TEnum t) {
+        /*
+            Map<String, Object> hm = new LinkedHashMap<>();
+            hm.put("A", tRepo.teamsAsil(0) );
+            hm.put("B", tRepo.teamsAsil(1) );
+            return new ResponseEntity( hm , HttpStatus.OK );
+         */
         return new ResponseEntity( tRepo.teamsAsil( t.ordinal() ), HttpStatus.OK );
     }
 
@@ -28,6 +32,13 @@ public class TeamsCreateRestController {
     public ResponseEntity backUpCreate(@RequestParam TEnum t) {
         return new ResponseEntity( tRepo.teamsYedek( t.ordinal() ), HttpStatus.OK );
     }
+
+    /*
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity notValidException( MethodArgumentNotValidException ex ) {
+        return new ResponseEntity( null , HttpStatus.BAD_REQUEST );
+    }
+     */
 
 
 }
